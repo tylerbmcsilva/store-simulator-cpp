c6ed3831df173f1855959e0be1ba9e250c1de2cb
Tyler Silva <me@tylerbsilva.com>
2017-03-12 13:00:16 -0700

    Initial Commit

---
+++ store-simulator-cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  store-simulator-cpp
+//
+//  Created by Tyler Silva on 3/12/17.
+//  Copyright © 2017 Tyler McSilva. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
